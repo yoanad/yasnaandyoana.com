b15b762423c06adbf00f3166acb6f57d4c56fdf3
yoanad <yoana.dimova@gmail.com>
2022-12-11 11:24:50 +0100

    Updates

---
+++ precache-manifest.2c03c38f1b4fd18f80d97cb146def12d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "13781fada9037687f6c6",
+    "url": "/yasnaandyoana.com/static/css/main.0711963b.chunk.css"
+  },
+  {
+    "revision": "13781fada9037687f6c6",
+    "url": "/yasnaandyoana.com/static/js/main.ed1c5166.chunk.js"
+  },
+  {
+    "revision": "59204249892c5e919b67",
+    "url": "/yasnaandyoana.com/static/js/runtime~main.f4e2e601.js"
+  },
+  {
+    "revision": "1140456f61a24275163b",
+    "url": "/yasnaandyoana.com/static/js/2.d3c549cd.chunk.js"
+  },
+  {
+    "revision": "7b381c63b71f07fd5d54",
+    "url": "/yasnaandyoana.com/static/js/3.bfb36310.chunk.js"
+  },
+  {
+    "revision": "601d35e2ac50565c1b4449c79bdc7eb9",
+    "url": "/yasnaandyoana.com/static/media/logo.601d35e2.svg"
+  },
+  {
+    "revision": "0e9773596e988066f0a8899ea6e2259d",
+    "url": "/yasnaandyoana.com/static/media/Yasna.0e977359.svg"
+  },
+  {
+    "revision": "660dbc5ed83fe34c5450ad127358fb09",
+    "url": "/yasnaandyoana.com/static/media/Yoana.660dbc5e.svg"
+  },
+  {
+    "revision": "83a54aafed21cb00f37c41c14cfc3eff",
+    "url": "/yasnaandyoana.com/index.html"
+  }
+];
