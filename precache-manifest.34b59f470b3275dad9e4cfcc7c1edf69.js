f6c8fbaf5df93cee65e992369dd988589b57a87c
yoanad <yoana.dimova@gmail.com>
2022-12-11 21:02:46 +0100

    Updates

---
+++ precache-manifest.34b59f470b3275dad9e4cfcc7c1edf69.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "13781fada9037687f6c6",
+    "url": "/static/css/main.0711963b.chunk.css"
+  },
+  {
+    "revision": "13781fada9037687f6c6",
+    "url": "/static/js/main.ed1c5166.chunk.js"
+  },
+  {
+    "revision": "72c960c1f3ed0db48cbc",
+    "url": "/static/js/runtime~main.9b51b16e.js"
+  },
+  {
+    "revision": "1140456f61a24275163b",
+    "url": "/static/js/2.d3c549cd.chunk.js"
+  },
+  {
+    "revision": "7b381c63b71f07fd5d54",
+    "url": "/static/js/3.bfb36310.chunk.js"
+  },
+  {
+    "revision": "601d35e2ac50565c1b4449c79bdc7eb9",
+    "url": "/static/media/logo.601d35e2.svg"
+  },
+  {
+    "revision": "0e9773596e988066f0a8899ea6e2259d",
+    "url": "/static/media/Yasna.0e977359.svg"
+  },
+  {
+    "revision": "660dbc5ed83fe34c5450ad127358fb09",
+    "url": "/static/media/Yoana.660dbc5e.svg"
+  },
+  {
+    "revision": "6675a28b3931845ee1931919fe9bcedf",
+    "url": "/index.html"
+  }
+];
