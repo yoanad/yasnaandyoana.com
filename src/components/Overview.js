81e179fdb82e5c50456d6de3156edb0c56bdbaaa
yoanad <yoana.dimova@gmail.com>
2023-06-28 18:36:55 +0300

    Fix image

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -6,14 +6,14 @@ import { useMediaQuery } from 'react-responsive';
 
 
 const Overview = () => {
-    const isDesktopOrLaptop = useMediaQuery({ minWidth: 1224 })
+    const isMobile = useMediaQuery({ maxWidth: 480 })
 
     return (
         <section className="overview-container" id="products">
             <h1>Give your skin a nourishing clean!</h1>
             <p className='text'>Start today with our soap heroes!</p>
             <div className='image-wrapper'>
-                <img src={isDesktopOrLaptop ? overlay : overlay_mobile} />
+                <img src={isMobile ? overlay_mobile : overlay} />
             </div>
             <div className='cta-wrapper'>
                 <h2>Did you find the right one for YOU?</h2>
