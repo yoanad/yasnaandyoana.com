168e4ac9db4163253adff5c5f25a26084ae45c10
yoanad <yoana.dimova@gmail.com>
2023-06-28 19:27:16 +0300

    Try to make form work

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -3,14 +3,19 @@ import './ContactForm.css';
 
 const ContactForm = () => {
     return (
-        <section className="contact-form" id="contact" netlify>
+        <section className="contact-form" id="contact">
             <h1>We sparked your interest? Talk to us!</h1>
             <p>We are happy you landed here! Don't hesitate to ask us any questions about products and orders!</p>
-            <form>
+            <form name="contact" netlify netlify-honeypot="bot-field" hidden className='hidden-form'>
+                <input type="email" name="email" />
+                <textarea name="message"></textarea>
+            </form>
+            <form name="contact" method="post">
+                <input type="hidden" name="contact" value="contact" />
                 <label for="email">Your email</label>
-                <input type="email" id="email" />
+                <input type="email" id="email" required />
                 <label for="message">What do you want to share with us (orders, product wishes, ideas, questions)?</label>
-                <textarea id="message"></textarea>
+                <textarea id="message" required></textarea>
                 <button type="submit">Submit</button>
             </form>
         </section>
