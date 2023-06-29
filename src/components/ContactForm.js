f1599e91a7f2a26c4040488e5924b5180f44d840
yoanad <yoana.dimova@gmail.com>
2023-06-29 11:17:11 +0300

    Change onSubmit

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -18,6 +18,7 @@ const ContactForm = () => {
     }
 
     const onSubmit = (e) => {
+        e.preventDefault();
         fetch("/", {
             method: "POST",
             headers: { "Content-Type": "application/x-www-form-urlencoded" },
@@ -25,8 +26,6 @@ const ContactForm = () => {
         })
             .then(() => setIsSubmitted(true))
             .catch(error => alert(error));
-
-        e.preventDefault();
     }
 
 
@@ -35,16 +34,16 @@ const ContactForm = () => {
             <h1>We sparked your interest? Talk to us!</h1>
             <p>We are happy you landed here! Don't hesitate to ask us any questions about products and orders!</p>
             <form name="contact" netlify netlify-honeypot="bot-field" hidden className='hidden-form'>
-                <input type="email" name="email" value={formData.email} />
-                <textarea name="message" value={formData.message}></textarea>
+                <input type="email" name="email" />
+                <textarea name="message"></textarea>
             </form>
-            {isSubmitted ? <h2>Form succesfully submitted!</h2> : (<form name="contact" method="post">
+            {isSubmitted ? <h2>Form succesfully submitted!</h2> : (<form onSubmit={onSubmit}>
                 <input type="hidden" name="contact" value="contact" />
                 <label for="email">Your email</label>
                 <input type="email" id="email" value={formData.email} onChange={onChange} required />
                 <label for="message">What do you want to share with us (orders, product wishes, ideas, questions)?</label>
                 <textarea id="message" required value={formData.message} onChange={onChange}></textarea>
-                <button type="submit" onSubmit={onSubmit}>Submit</button>
+                <button type="submit">Submit</button>
                 {/* <span className='checkbox-container'>
                     <input type="checkbox" name="checkbox" />
                     <label for="checkbox">Keep me up to date about the official release and other news!</label>
