03ad40340707c92b045b94edb5c57268e2872beb
yoanad <yoana.dimova@gmail.com>
2023-06-29 11:06:32 +0300

    Try to make form work

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,23 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ContactForm.css';
 
+const encode = (data) => {
+    return Object.keys(data)
+        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+        .join("&");
+}
+
 const ContactForm = () => {
+    const [isSubmitted, setIsSubmitted] = useState(false);
+    const [formData, setFormData] = useState({ email: null, message: null });
+    console.log(isSubmitted)
+
+    const onChange = (e) => {
+        setFormData({ [e.target.name]: e.target.value });
+
+    }
+
+    const onSubmit = (e) => {
+        fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: encode({ "form-name": "contact", ...formData })
+        })
+            .then(() => setIsSubmitted(true))
+            .catch(error => alert(error));
+
+        e.preventDefault();
+    }
+
+
     return (
         <section className="contact-form" id="contact">
             <h1>We sparked your interest? Talk to us!</h1>
             <p>We are happy you landed here! Don't hesitate to ask us any questions about products and orders!</p>
             <form name="contact" netlify netlify-honeypot="bot-field" hidden className='hidden-form'>
-                <input type="email" name="email" />
-                <textarea name="message"></textarea>
+                <input type="email" name="email" value={formData.email} />
+                <textarea name="message" value={formData.message}></textarea>
             </form>
-            <form name="contact" method="post">
+            {isSubmitted ? <h2>Form succesfully submitted!</h2> : (<form name="contact" method="post">
                 <input type="hidden" name="contact" value="contact" />
                 <label for="email">Your email</label>
-                <input type="email" id="email" required />
+                <input type="email" id="email" value={formData.email} onChange={onChange} required />
                 <label for="message">What do you want to share with us (orders, product wishes, ideas, questions)?</label>
-                <textarea id="message" required></textarea>
-                <button type="submit">Submit</button>
-            </form>
+                <textarea id="message" required value={formData.message} onChange={onChange}></textarea>
+                <button type="submit" onSubmit={onSubmit}>Submit</button>
+                {/* <span className='checkbox-container'>
+                    <input type="checkbox" name="checkbox" />
+                    <label for="checkbox">Keep me up to date about the official release and other news!</label>
+                </span> */}
+            </form>)}
         </section>
     );
 }
