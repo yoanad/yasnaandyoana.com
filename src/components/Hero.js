cb243bfaae4a574349a5b01a11d8e390afac449c
Felix Hamann <felix.hamann@satellytes.com>
2023-06-30 13:26:30 +0200

    feat: restrict max-width of content and add paddings

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,20 +3,28 @@ import './Hero.css';
 import logo from '../assets/logo.svg'
 
 const Hero = () => {
-    return (
-        <header>
-            <div className='navigation-container'>
-                <img src={logo} alt="Logo" />
-                <nav>
-                    <ul>
-                        <li><a href="#products">PRODUCTS</a></li>
-                        <li><a href="#about">ABOUT US</a></li>
-                        <li><a href="#contact">CONTACT</a></li>
-                    </ul>
-                </nav>
-            </div>
-        </header>
-    );
-}
+  return (
+    <header className="header">
+      <div className="content-container">
+        <div className="navigation-container">
+          <img src={logo} alt="Logo" />
+          <nav>
+            <ul>
+              <li>
+                <a href="#products">PRODUCTS</a>
+              </li>
+              <li>
+                <a href="#about">ABOUT US</a>
+              </li>
+              <li>
+                <a href="#contact">CONTACT</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+};
 
 export default Hero;
--- src/components/Overview.js
+++ src/components/Overview.js
@@ -6,21 +6,23 @@ import { useMediaQuery } from 'react-responsive';
 
 
 const Overview = () => {
-    const isMobile = useMediaQuery({ maxWidth: 480 })
+  const isMobile = useMediaQuery({ maxWidth: 480 });
 
-    return (
-        <section className="overview-container" id="products">
-            <h1>Give your skin a nourishing clean!</h1>
-            <p className='text'>Start today with our soap heroes!</p>
-            <div className='image-wrapper'>
-                <img src={isMobile ? overlay_mobile : overlay} />
-            </div>
-            <div className='cta-wrapper'>
-                <h2>Did you find the right one for YOU?</h2>
-                <a href="#contact">Contact us!</a>
-            </div>
-        </section>
-    );
-}
+  return (
+    <section className="overview-container section" id="products">
+      <div className="content-container">
+        <h1>Give your skin a nourishing clean!</h1>
+        <p className="text">Start today with our soap heroes!</p>
+        <div className="image-wrapper">
+          <img src={isMobile ? overlay_mobile : overlay} />
+        </div>
+        <div className="cta-wrapper">
+          <h2>Did you find the right one for YOU?</h2>
+          <a href="#contact">Contact us!</a>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Overview;
